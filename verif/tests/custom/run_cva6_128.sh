40d01b78528b3d8e9ce4ea3c26cfb3b50ad1f64e
Vincent Verdillon <vincent.verdillon@univ-grenoble-alpes.fr>
2026-07-29 13:41:31 +0200

    Refactor launch scripts
    This commit refactors bash scripts to run the CVA6 to simplify them.
    And it uniformizes some basic assembly programs.

--- verif/tests/custom/run_cva6_128.sh
+++ verif/tests/custom/run_cva6_128.sh
@@ -5,8 +5,7 @@
 #
 # Authors       : Vincent Verdillon
 # Creation Date : June, 2026
-# Description   : Sanitize Makefile
-# History       :
+# Description   : Run a source file in RISC-V 128 bits architecture.
 
 # check where are the tools
 if ! [ -n "$RISCV" ]; then
@@ -22,11 +21,11 @@ if [[ "$src0" == "--help" || -z "$src0" ]]; then
     exit 0
 fi
 
-cd verif/sim
+# convert the relative path to an absolute path
+src0=$(realpath "$src0")
 
 srcA=(
-    # /cva6_128/verif/tests/custom/common/syscalls.c
-    /cva6_128/verif/tests/custom/common/crt0.S
+    $(realpath "verif/tests/custom/common/crt0.S")
 )
 cflags=(
     -fno-tree-loop-distribute-patterns
@@ -39,8 +38,8 @@ cflags=(
     -O0 --no-inline
     -Wno-implicit-function-declaration
     -Wno-implicit-int
-    -I /cva6_128/verif/tests/custom/env/
-    -I /cva6_128/verif/tests/custom/common/
+    -I$(realpath "verif/tests/custom/env/")
+    -I$(realpath "verif/tests/custom/common/")
     -DNOPRINT
 )
 carch=(
@@ -48,39 +47,10 @@ carch=(
     -mabi=llp128
 )
 
-# Generate the output name
-basename_with_ext=$(basename "$src0")
-name="${basename_with_ext%.*}"
+cd verif/sim
 
-# create the output directory
-current_date=$(date +%Y-%m-%d)
-output_dir="/cva6_128/verif/sim/out_${current_date}/directed_tests/"
-mkdir -p "$output_dir"
-
-echo "Compile to ELF..."
-
-# set -x
-
-# Compile program the programme
-$RISCV_CC "$src0" \
-    -I /cva6_128/verif/sim/dv/user_extension \
-    -T/cva6_128/verif/sim/../../config/gen_from_riscv_config/linker/link.ld \
-    "${srcA[@]}" \
-    "${cflags[@]}" \
-    -o $output_dir$name \
-    "${carch[@]}"
-
-# set +x
-
-echo "Convert ELF to BIN to be used by the RTL simulation..."
-
-# get the thost addr to discuss with the host and cut it to avoid 128 bits addr
-tohost_addr=$($RISCV/bin/${CV_SW_PREFIX}nm -B $output_dir$name | grep -w tohost | cut -d' ' -f1 | cut -c17-32)
-echo "tohost address: $tohost_addr"
-
-# Works but do not uses binary, uses ELF, nobody can read ELF 128 bits
-# need to convert ELF to BIN with cva6.py option
-        # --steps=gen,gcc_compile,iss_sim,iss_cmp \
+# Run the CVA6 simulation with the specified parameters.
+# It works only if the script is executed from the root of the project.
 python3 cva6.py \
         --target cv128a6_im_sv39_hpdcache_wb \
         --hwconfig_opts="$DV_HWCONFIG_OPTS" \
@@ -92,11 +62,4 @@ python3 cva6.py \
         --gcc_opts "${srcA[*]} ${cflags[*]}" \
         $DV_OPTS
 
-# run verilator
-# $ROOT_PROJECT/work-ver/Variane_testharness $ELF_BIN_DATA \
-#                                            ++$ELF_BIN_DATA \
-#                                            +elf_file=$ELF_BIN_DATA \
-#                                            +debug_disable=1 \
-#                                            +core_name=cv128 \
-#                                            +tohost_addr=$tohost_addr
-
+cd ../../
